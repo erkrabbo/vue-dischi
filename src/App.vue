d7f68f8cbaa8baf6cfa80f77dd82820eb48aeca1
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-24 14:54:07 +0100

    da finire

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <img src="https://cantabrialabsdifacooper.it/wp-content/uploads/2021/03/png-clipart-spotify-logo-spotify-computer-icons-podcast-music-apps-miscellaneous-angle.png" alt="Spotify logo" width="50">
+    </header>
+    <main>
+      <div class="container">
+        <div class="row row-cols-2 row-cols-md-4 row-cols-lg-5">
+          <spoty-card v-for="card in cards" :key="card.title" :card-data="card" />
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import SpotyCard from './components/SpotyCard.vue'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      cards: null
+    }
+  },
   components: {
-    HelloWorld
+    SpotyCard
+  },
+  created () {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then(result => {
+        this.cards = result.data.response
+        console.log(this.cards)
+      })
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/bootstrap/scss/bootstrap";
 </style>
---
+++ src/components/SpotyCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col">
+    <div class="card">
+      <img class="card-img-top" :src="cardData.poster" :alt="cardData.title">
+      <h2 class="card-title">{{ cardData.title}}</h2>
+      <div class="card-text">
+        <p>{{ cardData.author}}</p>
+        <p>{{ cardData.year }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SpotyCard',
+  props: {
+    cardData: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
